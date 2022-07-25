7c1cff0ca3e8324811dcce8695fd35bcdaf3fb2f
Toutchy kipik <toutchy.kipik@pm.me>
2022-07-25 14:11:18 +0200

    sticfiles

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -133,8 +133,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR / '/static',
+    BASE_DIR / 'static',
 ]
+STATIC_FILES_STORAGE = 'portfolio.storage.StaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-import portfolio_back
 from portfolio_back import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
